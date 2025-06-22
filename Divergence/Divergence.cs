395a058417d00ca55ae1e2e733b17b818c439dfa
artificialparanoia <artificialparanoia@protonmail.com>
2025-06-22 03:39:52 -0400

    Enhance Divergence integration
    - Integrates Patch.cs from Enhance Divergence into Divergence itself.
    - Turns Divergence into a code mod.

---
+++ Divergence/Divergence.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using OWML.Common;
+using OWML.ModHelper;
+
+namespace Divergence
+{
+    public class Divergence : ModBehaviour
+    {
+        public static Divergence Instance;
+        public INewHorizons NewHorizons;
+
+        public void Awake()
+        {
+            Instance = this;
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public void Start()
+        {
+            // Get the New Horizons API and load configs
+            NewHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+            NewHorizons.LoadConfigs(this);
+            ModHelper.Console.WriteLine($"My mod {nameof(Divergence)} is loaded!", MessageType.Success);
+            Patch.Initialize();
+        }
+    }
+
+}
